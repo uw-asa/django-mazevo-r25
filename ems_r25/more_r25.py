553380d7e03f8b02c0618a2c8468e6d056e4e996
Bradley Bell <bradleyb@uw.edu>
2019-06-19 15:07:41 -0700

    log new event id

--- ems_r25/more_r25.py
+++ ems_r25/more_r25.py
@@ -328,6 +328,7 @@ def update_event(event):
         enode = event_tree.xpath("r25:event", namespaces=nsmap)[0]
 
         event.event_id = enode.xpath("r25:event_id", namespaces=nsmap)[0].text
+        logger.debug("created new event %s" % event.event_id)
 
         # initialize some things that aren't kept in the uw_r25 model
         update_value(enode, 'node_type', 'E')

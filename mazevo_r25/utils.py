37904d49d5dd10fae24bb5cf6fc90fb68d35f05d
Bradley Bell <bradleyb@uw.edu>
2024-04-17 02:15:48 -0700

    no external ids in mazevo

--- mazevo_r25/utils.py
+++ mazevo_r25/utils.py
@@ -1,12 +1,10 @@
 import logging
+from urllib.parse import quote
 
-from django.conf import settings
-from lxml import etree
-from lxml.etree import XMLSyntaxError
-from urllib3.exceptions import MaxRetryError
-from uw_r25 import nsmap, get_resource
+from uw_r25 import get_resource
+from uw_r25.spaces import spaces_from_xml
 
-from .more_r25 import put_resource, R25ErrorException
+from .models import MazevoRoomSpace
 
 
 logger = logging.getLogger(__name__)
@@ -20,69 +18,16 @@ def update_get_space_ids(mazevo_rooms):
     :return: A dictionary of Room.id: space_id
     """
     space_ids = {}
+
     for room in mazevo_rooms:
-        if room.active and room.external_reference is not None:
-            space_ids[room.id] = room.external_reference
-
-    # while we're here, update the R25 saved search that we'll use
-    query_url = "space_search.xml?query_id=%s" % settings.MAZEVO_R25_SPACE_QUERY
-
-    try:
-        r25_query_tree = get_resource(query_url)
-    except (R25ErrorException, MaxRetryError, XMLSyntaxError) as ex:
-        logger.warning("R25 error while retrieving space search: %s" % ex)
-
-        return space_ids
-
-    snode = r25_query_tree.xpath("r25:search", namespaces=nsmap)[0]
-    snode.attrib["status"] = "mod"
-
-    step = snode.xpath("r25:step", namespaces=nsmap)[0]
-    step.attrib["status"] = "del"
-
-    query_modified = False
-
-    found_space_ids = []
-    for pnode in step.xpath("r25:step_param", namespaces=nsmap):
-        space_id = pnode.xpath("r25:space_id", namespaces=nsmap)[0].text
-        found_space_ids.append(space_id)
-        pnode.attrib["status"] = "del"
-
-        if space_id not in space_ids.values():
-            query_modified = True
-
-    # replace with new search step
-    step = etree.Element("{%s}step" % nsmap["r25"], attrib={"status": "new"})
-    node = etree.Element("{%s}step_number" % nsmap["r25"])
-    node.text = "1"
-    step.append(node)
-    node = etree.Element("{%s}step_type_id" % nsmap["r25"])
-    node.text = "407"  # Spaces
-    step.append(node)
-    node = etree.Element("{%s}qualifier" % nsmap["r25"])
-    node.text = "1"  # Include Any
-    step.append(node)
-    snode.append(step)
-
-    step_param_nbr = 0
-    for space_id in space_ids.values():
-        step_param_nbr += 1
-        param = etree.Element("{%s}step_param" % nsmap["r25"])
-        node = etree.Element("{%s}step_param_nbr" % nsmap["r25"])
-        node.text = str(step_param_nbr)
-        param.append(node)
-        node = etree.Element("{%s}space_id" % nsmap["r25"])
-        node.text = space_id
-        param.append(node)
-        step.append(param)
-
-        if space_id not in found_space_ids:
-            query_modified = True
-
-    if query_modified:
+        logger.info("Mazevo room %s" % room.description)
         try:
-            put_resource(query_url, etree.tostring(r25_query_tree))
-        except (R25ErrorException, MaxRetryError, XMLSyntaxError) as ex:
-            logger.warning("R25 error while updating space search: %s" % ex)
+            url = "spaces.xml"
+            url += "?short_name={}".format(quote(room.description))
+            space = spaces_from_xml(get_resource(url))[0]
+        except Exception as ex:
+            logger.warning("R25 error while retrieving space %s: %s" % (room.description, ex))
 
-    return space_ids
+        space_ids[room.id] = space.space_id
+
+    return MazevoRoomSpace.objects.in_bulk()

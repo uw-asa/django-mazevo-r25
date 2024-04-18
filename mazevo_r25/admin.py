94e4547d67d38b270ad811ecee0f58f122b7a01d
Bradley Bell <bradleyb@uw.edu>
2024-04-18 16:39:01 -0700

    start custom admin

--- mazevo_r25/admin.py
+++ mazevo_r25/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.MazevoRoomSpace)
+class MazevoRoomSpaceAdmin(admin.ModelAdmin):
+    list_display = ('room_id', 'space_id', 'date_changed')
+    list_editable = ('space_id',)
+
+admin.site.register(models.MazevoRoomSpace, MazevoRoomSpaceAdmin)

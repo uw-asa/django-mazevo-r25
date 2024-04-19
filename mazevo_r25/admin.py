d89d5ec8c9e59d83901df3697a46def322183b87
Bradley Bell <bradleyb@uw.edu>
2024-04-18 23:46:51 -0700

    Show room name on admin form

--- mazevo_r25/admin.py
+++ mazevo_r25/admin.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib import admin
 from django.db import models
 from django.forms.widgets import Select
+from uw_mazevo.public_configuration import PublicConfiguration
 
 from .more_r25 import get_space_list
 from .models import MazevoRoomSpace
@@ -9,6 +10,15 @@ from .models import MazevoRoomSpace
 class MazevoRoomSpaceForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(MazevoRoomSpaceForm, self).__init__(*args, **kwargs)
+
+        room_id_widget = forms.Select()
+        room_id_widget.choices = []
+        for room in PublicConfiguration().get_rooms():
+            room_id_widget.choices.append((room.id, room.description))
+
+        self.fields['room_id'].label = 'Mazevo Room'
+        self.fields['room_id'].widget = room_id_widget
+        self.fields['room_id'].disabled = True
 
         space_id_widget = forms.Select()
         space_id_widget.choices = get_space_list()

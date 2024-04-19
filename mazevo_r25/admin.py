00d08edae3caa438056a41f0b5fe63a458a7fdf7
Bradley Bell <bradleyb@uw.edu>
2024-04-19 01:25:01 -0700

    friendlier room/space editing

--- mazevo_r25/admin.py
+++ mazevo_r25/admin.py
@@ -1,11 +1,7 @@
 from django import forms
 from django.contrib import admin
-from django.db import models
-from django.forms.widgets import Select
-from uw_mazevo.public_configuration import PublicConfiguration
 
-from .more_r25 import get_space_list
-from .models import MazevoRoomSpace
+from .models import MazevoRoomSpace, room_names, space_names
 
 class MazevoRoomSpaceForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -13,24 +9,23 @@ class MazevoRoomSpaceForm(forms.ModelForm):
 
         room_id_widget = forms.Select()
         room_id_widget.choices = []
-        for room in PublicConfiguration().get_rooms():
-            room_id_widget.choices.append((room.id, room.description))
+        for room in room_names:
+            room_id_widget.choices.append((room, "{} ({})".format(room, room_names[room])))
 
         self.fields['room_id'].label = 'Mazevo Room'
         self.fields['room_id'].widget = room_id_widget
         self.fields['room_id'].disabled = True
 
         space_id_widget = forms.Select()
-        space_id_widget.choices = get_space_list()
-        space_id_widget.choices.insert(0, (None, 'Not Defined'))
+        space_id_widget.choices = [(None, 'Not Defined')]
+        for space in space_names:
+            space_id_widget.choices.append((space, "{} ({})".format(space, space_names[space])))
 
         self.fields['space_id'].label = 'R25 Space'
         self.fields['space_id'].widget = space_id_widget
 
-
 class MazevoRoomSpaceAdmin(admin.ModelAdmin):
-    list_display = ('room_id', 'space_id', 'date_changed')
-    list_editable = ('space_id',)
+    list_display = ('room_id', 'space_id', 'room_name', 'space_name', 'date_changed')
     form = MazevoRoomSpaceForm
 
 admin.site.register(MazevoRoomSpace, MazevoRoomSpaceAdmin)
--- mazevo_r25/models.py
+++ mazevo_r25/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from uw_mazevo.public_configuration import PublicConfiguration
+
+from .more_r25 import get_space_list
+
+room_names = {}
+for room in PublicConfiguration().get_rooms():
+    room_names[room.id] = room.description
+
+space_names = dict(get_space_list())
 
 class MazevoRoomSpace(models.Model):
     """
@@ -7,3 +16,14 @@ class MazevoRoomSpace(models.Model):
     room_id = models.PositiveIntegerField(primary_key=True)
     space_id = models.PositiveIntegerField(unique=True, null=True, default=None)
     date_changed = models.DateTimeField(auto_now=True)
+
+    @property
+    def room_name(self):
+        return room_names[self.room_id]
+
+    @property
+    def space_name(self):
+        try:
+            return space_names[self.space_id]
+        except:
+            return 'Invalid'

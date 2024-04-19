ee32a4294675d4d971d64315260fdbb3a41e7860
Bradley Bell <bradleyb@uw.edu>
2024-04-18 20:12:54 -0700

    custom admin form

--- mazevo_r25/admin.py
+++ mazevo_r25/admin.py
@@ -1,9 +1,26 @@
+from django import forms
 from django.contrib import admin
+from django.db import models
+from django.forms.widgets import Select
 
-from . import models
+from .more_r25 import get_space_list
+from .models import MazevoRoomSpace
+
+class MazevoRoomSpaceForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(MazevoRoomSpaceForm, self).__init__(*args, **kwargs)
+
+        space_id_widget = forms.Select()
+        space_id_widget.choices = get_space_list()
+        space_id_widget.choices.insert(0, (None, 'Not Defined'))
+
+        self.fields['space_id'].label = 'R25 Space'
+        self.fields['space_id'].widget = space_id_widget
+
 
 class MazevoRoomSpaceAdmin(admin.ModelAdmin):
     list_display = ('room_id', 'space_id', 'date_changed')
     list_editable = ('space_id',)
+    form = MazevoRoomSpaceForm
 
-admin.site.register(models.MazevoRoomSpace, MazevoRoomSpaceAdmin)
+admin.site.register(MazevoRoomSpace, MazevoRoomSpaceAdmin)

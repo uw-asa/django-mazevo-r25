aca5e9146917c0d12fc007236a24af0d9e253388
Bradley Bell <bradleyb@uw.edu>
2024-04-20 00:33:06 -0700

    more test

--- mazevo_r25/tests/test_more_r25.py
+++ mazevo_r25/tests/test_more_r25.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 
-from mazevo_r25.more_r25 import get_space_list
+from mazevo_r25.more_r25 import get_space_by_short_name, get_space_list
 
 
 class TestMoreR25(TestCase):
+
+    def test_get_space_by_short_name(self):
+        space = get_space_by_short_name("GLD 100A")
+        self.assertEquals(space.space_id, "1001")
 
     def test_get_space_list(self):
         spaces = get_space_list()
@@ -12,4 +16,4 @@ class TestMoreR25(TestCase):
         self.assertEquals(len(space), 2)
         (id, name) = space
         self.assertEquals(id, 1002)
-        self.assertAlmostEquals(name, 'JHN 303')
+        self.assertEquals(name, "JHN 303")

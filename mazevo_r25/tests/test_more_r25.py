e522fba20386f820177eefe15ce750281a14705e
Bradley Bell <bradleyb@uw.edu>
2024-04-25 18:15:28 -0700

    assertEquals => assertEqual

--- mazevo_r25/tests/test_more_r25.py
+++ mazevo_r25/tests/test_more_r25.py
@@ -11,16 +11,16 @@ class TestMoreR25(TestCase):
 
     def test_get_space_by_short_name(self):
         space = get_space_by_short_name("GLD 100A")
-        self.assertEquals(space.space_id, "1001")
+        self.assertEqual(space.space_id, "1001")
 
     def test_get_space_list(self):
         spaces = get_space_list()
-        self.assertEquals(len(spaces), 3)
+        self.assertEqual(len(spaces), 3)
         space = spaces[2]
-        self.assertEquals(len(space), 2)
+        self.assertEqual(len(space), 2)
         (id, name) = space
-        self.assertEquals(id, 1002)
-        self.assertEquals(name, "JHN 303")
+        self.assertEqual(id, 1002)
+        self.assertEqual(name, "JHN 303")
 
     def test_get_event_type_list(self):
         types = get_event_type_list()  # only cabinets

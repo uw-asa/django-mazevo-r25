25dd4e6cdd7cd0f08939f24c1c6ded3ece2d3b61
Bradley Bell <bradleyb@uw.edu>
2024-04-25 18:09:04 -0700

    add test for event type list

--- mazevo_r25/tests/test_more_r25.py
+++ mazevo_r25/tests/test_more_r25.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from mazevo_r25.more_r25 import get_space_by_short_name, get_space_list
+from mazevo_r25.more_r25 import get_event_type_list, get_space_by_short_name, get_space_list
 
 
 class TestMoreR25(TestCase):
@@ -17,3 +17,10 @@ class TestMoreR25(TestCase):
         (id, name) = space
         self.assertEquals(id, 1002)
         self.assertEquals(name, "JHN 303")
+
+    def test_get_event_type_list(self):
+        types = get_event_type_list()  # only cabinets
+        self.assertEqual(len(types), 2, "cabinet event type count")
+
+        types = get_event_type_list(all_types="T")
+        self.assertEqual(len(types), 7, "event type count")

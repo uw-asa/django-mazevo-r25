31316f62afaad424025985267ba7376f247735e1
Bradley Bell <bradleyb@uw.edu>
2025-01-15 15:25:57 -0800

    implement our own reservations_from_xml
    We need more details than the existing model has.

--- mazevo_r25/more_r25.py
+++ mazevo_r25/more_r25.py
@@ -9,9 +9,8 @@ from restclients_core.util.retry import retry
 from uw_r25 import nsmap, get_resource
 from uw_r25.dao import R25_DAO
 from uw_r25.events import events_from_xml
-from uw_r25.models import Event
-from uw_r25.reservations import reservations_from_xml
-from uw_r25.spaces import spaces_from_xml
+from uw_r25.models import Event, Reservation
+from uw_r25.spaces import space_reservation_from_xml, spaces_from_xml
 
 
 logger = logging.getLogger(__name__)
@@ -640,6 +639,75 @@ def delete_favorite(object_type, object_id):
     result = delete_resource(url)
 
     return result
+
+
+def reservations_from_xml(tree):
+    try:
+        profile_name = tree.xpath("r25:profile_name", namespaces=nsmap)[0].text
+    except Exception:
+        profile_name = None
+
+    reservations = []
+    for node in tree.xpath("r25:reservation", namespaces=nsmap):
+        reservation = Reservation()
+        reservation.reservation_id = node.xpath("r25:reservation_id",
+                                                namespaces=nsmap)[0].text
+        reservation.start_datetime = node.xpath("r25:reservation_start_dt",
+                                                namespaces=nsmap)[0].text
+        reservation.end_datetime = node.xpath("r25:reservation_end_dt",
+                                              namespaces=nsmap)[0].text
+        reservation.state = node.xpath("r25:reservation_state",
+                                       namespaces=nsmap)[0].text
+        reservation.registered_count = node.xpath("r25:registered_count",
+                                                  namespaces=nsmap)[0].text
+        if profile_name:
+            reservation.profile_name = profile_name
+        else:
+            reservation.profile_name = node.xpath("r25:profile_name",
+                                                  namespaces=nsmap)[0].text
+
+        try:
+            pnode = node.xpath("r25:space_reservation", namespaces=nsmap)[0]
+            reservation.space_reservation = space_reservation_from_xml(pnode)
+        except IndexError:
+            reservation.space_reservation = None
+
+        try:
+            enode = node.xpath("r25:event", namespaces=nsmap)[0]
+            reservation.event_id = enode.xpath("r25:event_id",
+                                               namespaces=nsmap)[0].text
+            reservation.event_name = enode.xpath("r25:event_name",
+                                                 namespaces=nsmap)[0].text
+            reservation.event_title = enode.xpath("r25:event_title",
+                                                  namespaces=nsmap)[0].text
+
+            rnode = enode.xpath("r25:role", namespaces=nsmap)[0]
+            cnode = rnode.xpath("r25:contact", namespaces=nsmap)[0]
+            reservation.contact_name = cnode.xpath("r25:contact_name",
+                                                   namespaces=nsmap)[0].text
+            try:
+                anode = cnode.xpath("r25:address", namespaces=nsmap)[0]
+                reservation.contact_email = anode.xpath(
+                    "r25:email", namespaces=nsmap)[0].text
+            except IndexError:
+                reservation.contact_email = None
+
+            reservation.event_notes = None
+            for tnode in enode.xpath("r25:event_text", namespaces=nsmap):
+                if tnode.xpath("r25:text_type_id", namespaces=nsmap)[0].text == '2':
+                    reservation.event_notes = tnode.xpath(
+                        "r25:text", namespaces=nsmap)[0].text
+
+        except IndexError:
+            enode = tree.getparent()
+            reservation.event_id = enode.xpath("r25:event_id",
+                                               namespaces=nsmap)[0].text
+            reservation.event_name = enode.xpath("r25:event_name",
+                                                 namespaces=nsmap)[0].text
+
+        reservations.append(reservation)
+
+    return reservations
 
 
 @retry(DataFailureException, status_codes=RETRY_STATUS_CODES, logger=logger)
